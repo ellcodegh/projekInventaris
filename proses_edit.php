4cdfeccc564b584d65dcda464c5bb7fff5e2da5c
Rafael Rasya <112202407153@mhs.dinus.ac.id>
2026-03-11 13:49:50 +0700

    update validasi satuan, fix bug overwrite, dan prepared statement

--- proses_edit.php
+++ proses_edit.php
@@ -1,19 +1,20 @@
 <?php
 include "koneksi.php";
 
-$kode = mysqli_real_escape_string($koneksi, $_POST['kode_barang']);
-$nama = mysqli_real_escape_string($koneksi, $_POST['nama_barang']);
-htmlspecialchars($row['nama_barang']);
-
-$id         = $_POST['id'];
-$kode       = $_POST['kode_barang'];
-$nama       = $_POST['nama_barang'];
+$id         = $_POST['id_barang'];
+$kode       = mysqli_real_escape_string($koneksi, $_POST['kode_barang']);
+$nama       = mysqli_real_escape_string($koneksi, $_POST['nama_barang']);
 $satuan     = $_POST['satuan'];
 $hb         = $_POST['harga_beli'];
 $hj         = $_POST['harga_jual'];
 $jumlah     = $_POST['jumlah'];
 $tanggal    = $_POST['tanggal_masuk'];
 $ket        = $_POST['keterangan'];
+
+if (preg_match('/[0-9]/', $satuan)) {
+    echo "<script>alert('Satuan tidak boleh mengandung angka!'); window.history.back();</script>";
+    exit;
+}
 
 if($_FILES['foto']['name'] != ''){
     $foto = $_FILES['foto']['name'];
@@ -30,31 +31,25 @@ if($_FILES['foto']['name'] != ''){
         }
     move_uploaded_file($tmp, "upload/".$foto);
 
-    mysqli_query($koneksi, "UPDATE barang SET
-        kode_barang='$kode',
-        nama_barang='$nama',
-        satuan='$satuan',
-        harga_beli='$hb',
-        harga_jual='$hj',
-        jumlah='$jumlah',
-        tanggal_masuk='$tanggal',
-        keterangan='$ket',
-        foto='$foto'
-        WHERE id_barang='$id'
-    ");
-}else{
-    mysqli_query($koneksi, "UPDATE barang SET
-        kode_barang='$kode',
-        nama_barang='$nama',
-        satuan='$satuan',
-        harga_beli='$hb',
-        harga_jual='$hj',
-        jumlah='$jumlah',
-        tanggal_masuk='$tanggal',
-        keterangan='$ket'
-        WHERE id_barang='$id'
-    ");
-}
+        $sql    = "UPDATE barang SET kode_barang=?, nama_barang=?, satuan=?, harga_beli=?,
+        harga_jual=?, jumlah=?, tanggal_masuk=?, keterangan=?, foto=? WHERE id_barang=?";
+        $stmt   = $koneksi->prepare($sql);
+        $stmt->bind_param("ssssiisssi", $kode, $nama, $satuan, $hb, $hj, $jumlah, $tanggal, $ket, $nama_foto_baru, $id);
 
+        } else {
+            $sql    = "UPDATE barang SET kode_barang=?, nama_barang=?, satuan=?, harga_beli=?,
+            harga_jual=?, jumlah=?, tanggal_masuk=?, keterangan=? WHERE id_barang=?";
+            $stmt = $koneksi->prepare($sql);
+            $stmt->bind_param("ssssiissi", $kode, $nama, $satuan, $hb, $hj, $jumlah, $tanggal, $ket, $id);
+        }
+
+    if($stmt->execute()){
+        header("Location: index.php");
+        } else {
+            echo "Gagal update: " . $stmt->error;
+        }
+
+$stmt->close();
+$koneksi->close();
 header("Location: index.php");
 ?>
--- proses_tambah.php
+++ proses_tambah.php
@@ -1,18 +1,19 @@
 <?php
 include "koneksi.php";
 
-$kode = mysqli_real_escape_string($koneksi, $_POST['kode_barang']);
-$nama = mysqli_real_escape_string($koneksi, $_POST['nama_barang']);
-htmlspecialchars($_POST['nama_barang']);
-
-$kode       = $_POST['kode_barang'];
-$nama       = $_POST['nama_barang'];
+$kode       = mysqli_real_escape_string($koneksi, $_POST['kode_barang']);
+$nama       = mysqli_real_escape_string($koneksi, $_POST['nama_barang']);
 $satuan     = $_POST['satuan'];
 $hb         = $_POST['harga_beli'];
 $hj         = $_POST['harga_jual'];
 $jumlah     = $_POST['jumlah'];
 $tanggal    = $_POST['tanggal_masuk'];
 $ket        = $_POST['keterangan'];
+
+if (preg_match('/[0-9]/', $satuan)) {
+    echo "<script>alert('Satuan tidak boleh mengandung angka!'); window.history.back();</script>";
+    exit;
+}
 
 $foto = $_FILES['foto']['name'];
 $tmp = $_FILES['foto']['tmp_name'];
@@ -31,9 +32,21 @@ $tmp = $_FILES['foto']['tmp_name'];
 
 move_uploaded_file($tmp, "upload/".$foto);
 
-mysqli_query($koneksi, "INSERT INTO barang VALUES(
-'', '$kode','$nama','$satuan','$hb','$hj','$jumlah','$tanggal','$ket','$foto'
-)");
+$sql = "INSERT INTO barang (id_barang, kode_barang, nama_barang, satuan, harga_beli, harga_jual, jumlah, tanggal_masuk, keterangan, foto) 
+        VALUES ('', ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+$stmt = $koneksi->prepare($sql);
+
+$stmt->bind_param("ssssiisss", $kode, $nama, $satuan, $hb, $hj, $jumlah, $tanggal, $ket, $nama_foto_baru);
+
+if($stmt->execute()){
+    header("Location: index.php");
+} else {
+    echo "Error: " . $stmt->error;
+}
+
+$stmt->close();
+$koneksi->close();
 
 header("Location: index.php");
 ?>
