b8a09c62623d0eb6afe0f8e41ef6e529a990e011
Rafael Rasya <112202407153@mhs.dinus.ac.id>
2026-03-29 21:06:42 +0700

    fix hapus foto di folder

--- hapus.php
+++ hapus.php
@@ -3,9 +3,18 @@ include "koneksi.php";
 
 $id = isset($_GET['id']) ? (int)$_GET['id'] : 0;
 
-$sql = "DELETE FROM barang WHERE id_barang = ?";
-$stmt = $koneksi->prepare($sql);
+$stmt = $koneksi->prepare("SELECT foto FROM barang WHERE id_barang = ?");
 $stmt->execute([$id]);
+$data = $stmt->fetch();
+
+if ($data) {
+    if (file_exists("upload/" . $data['foto'])) {
+        unlink("upload/" . $data['foto']);
+    }
+
+    $stmt = $koneksi->prepare("DELETE FROM barang WHERE id_barang = ?");
+    $stmt->execute([$id]);
+}
 
 header("Location: index.php");
 exit();
